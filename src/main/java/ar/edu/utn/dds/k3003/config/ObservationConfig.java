c9b205467a035735daaade46db08e67ae42fa7f8
unknown <abramlujan@gmail.com>
2025-09-02 22:55:27 -0300

    Configuracion metricas datadog y postgresql

---
+++ src/main/java/ar/edu/utn/dds/k3003/config/ObservationConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+
+@Configuration
+public class ObservationConfig {
+    
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+    
+}
